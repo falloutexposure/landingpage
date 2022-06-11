0339ca33cc2b0e81d591315ee3a4a99f31045eb3
memiux <me@memiux.com>
2022-06-11 08:55:36 -0500

    update favicons

--- main.js
+++ main.js
@@ -3,21 +3,13 @@ addEventListener('fetch', event => {
 });
 
 async function handleRequest(request) {
-	const url = new URL(request.url);
-	if (url.hostname == 'mta-sts.mushroomcloud.moe') {
-		if (url.pathname == '/.well-known/mta-sts.txt') {
-			request = new Request(request.url.replace('mta-sts.', ''), request);
-		} else {
-			return new Response('404', {status: 404});
-		}
-	}
 	response = await fetch(request);
 	response = new Response(response.body, response);
 	response.headers.append('Content-Security-Policy', "default-src 'self'");
 	response.headers.append('Permissions-Policy', 'interest-cohort=()');
-	response.headers.append('X-Content-Type-Options', 'nosniff');
+	// response.headers.append('X-Content-Type-Options', 'nosniff');
 	response.headers.append('X-Frame-Options', 'DENY');
 	response.headers.append('X-XSS-Protection', '0');
-	response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
+	// response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
 	return response;
 }
