ac004b834ae9e25b63ca61d20295a2931b98fc03
memiux <me@memiux.com>
2022-11-30 15:15:59 -0600

    Add mta-sts.txt file

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ addEventListener('fetch', event => {
 });
 
 async function handleRequest(request) {
-	response = await fetch(request);
+	let response = await fetch(request);
 	response = new Response(response.body, response);
 	response.headers.append('Content-Security-Policy', "default-src 'self'");
 	response.headers.append('Permissions-Policy', 'interest-cohort=()');
-	// response.headers.append('X-Content-Type-Options', 'nosniff');
 	response.headers.append('X-Frame-Options', 'DENY');
 	response.headers.append('X-XSS-Protection', '0');
+	// response.headers.append('X-Content-Type-Options', 'nosniff');
 	// response.headers.append('Referrer-Policy', 'strict-origin-when-cross-origin');
 	return response;
 }
